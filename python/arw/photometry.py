390cb2ad8c7e96c9194ea84585c6133e1ab72c57
afromero <rw.andres@gmail.com>
2015-03-12 13:49:24 -0700

    cookie cutter photometry

--- python/arw/photometry.py
+++ python/arw/photometry.py
@@ -19,54 +19,39 @@ rcParams['figure.facecolor']='white'
 #    g. correct the values to magnitudes
 #    h. derive time-series with root N errors.
 
-def estimate_total_light(obj, N_bkg):
+def estimate_total_light(obj, N_bkg, sigma_read, display=False):
   estimate = np.sum(obj.image -  N_bkg)
   uncertainty = np.sqrt(np.sum(obj.image**2)) 
-
-  
-  #COOKIE CUTTER, WORK IN PROGRESS
-  x_max, y_max = np.unravel_index(obj.image.argmax(), obj.image.shape)
-  r=[]
-  amp=[]
-  amp2=[]
-  for i in range(0,len(obj.image)):
-      for j in range(0,len(obj.image)):
-	r.append(np.sqrt((float(i)-x_max)**2 + (float(j)-y_max)**2))
-	amp.append(obj.image[i][j]-N_bkg)
-	amp2.append(obj.image[i][j]-N_bkg)
-  r,amp = zip(*sorted(zip(r, amp)))
-
-  # GET POINT WHERE TOTAL LIGHT CONTRIBUTION IS CHANGING BY < 0.01 %
-  # ONE REFINEMENT IS TO MASK VALUES LESS THAN ZERO (OR SQRT(N_BKG)), THEN INTEGRATE RADIALLY OUTWARD
-  v = diff(cumsum(amp))/cumsum(amp)[:-1]
-  #v_stp_arg = np.argmin((v-1.e-3)**2)
-  zero_crossings = np.where(np.diff(np.sign(v-1.e-3)))[0]
-  v_stp_arg = zero_crossings[0]
-  estimate=cumsum(amp)[v_stp_arg]
-  uncertainty=np.sqrt(cumsum(array(amp2)**2)[v_stp_arg])
-  #print 'r_stop', r[v_stp_arg]
-  
-  '''
-  #if(np.random.randint(0,100)==0):
-  if(2.5*uncertainty/estimate/log10(e) > 0.3):
-	  print '*x*', 2.5*uncertainty/estimate/log10(e)
-	  plt.figure()
-	  plt.imshow(obj.image,interpolation='none',cmap='jet')
-	  plt.colorbar()
-	  plt.figure()
-	  subplot(311)
-	  plt.plot(r,amp)
-	  subplot(312)
-	  plt.semilogy(r[:-1],diff(cumsum(amp))/cumsum(amp)[:-1])
-	  print v_stp_arg, v[v_stp_arg]
-	  plot([r[v_stp_arg],r[v_stp_arg]],[1.e-7,1.], '--')
-	  subplot(313)
-	  plt.plot(r,cumsum(amp))
-	  plt.show()
-  '''
-
-  #print 'e,u', estimate, uncertainty, N_bkg, 2.5*uncertainty/estimate/log10(e)
+  maskval = np.sqrt((3.*np.sqrt(N_bkg))**2 + sigma_read**2)
+  Hmasked = np.ma.masked_where((obj.image-N_bkg)<maskval,obj.image-N_bkg)
+  Hmasked2 = np.ma.masked_where((obj.image-N_bkg)<maskval,obj.image)
+  estimate = np.sum(Hmasked)
+  uncertainty = np.sqrt(np.sum(Hmasked2))
+  #if(display or uncertainty/estimate>3e-2):
+  if(display):
+	  print uncertainty/estimate
+	  figure()
+	  subplot(221)
+          m1 = np.min(obj.image)
+          m2 = np.max(obj.image)
+          m3 = np.min(obj.image-N_bkg)
+          m4 = np.max(obj.image-N_bkg)
+	  imshow(obj.image, interpolation='none', vmin=m1, vmax=m2)
+	  colorbar()
+	  title('Image')
+	  subplot(222)
+	  imshow(obj.image-N_bkg, interpolation='none', vmin=m3, vmax=m4)
+	  colorbar()
+	  title('Bkg Subtracted')
+	  subplot(223)
+	  imshow(Hmasked2, interpolation='none', vmin=m1, vmax=m2)
+	  colorbar()
+	  subplot(224)
+	  imshow(Hmasked, interpolation='none', vmin=m3, vmax=m4)
+	  colorbar()
+	  show()
   return estimate, uncertainty
+
 
 def magnitude(value):
   return -2.5*log10(value)
@@ -86,8 +71,9 @@ def APASS_zero_points(FM, APASS_table, APASS_rejects, display=False):
   # LOOP THROUGH APASS OBJECTS TO FILL LISTS. EXCLUDE PRE-ASSESSED APASS REJECTS LIST
   for k in range(0,len(APASS_table)):
     if(APASS_table[filt][k]!='NA' and APASS_table[filt_err][k]!='0' and k not in APASS_rejects): 
+	print 'APASS', k
 	obj = SourceImage(FM, APASS_table['radeg'][k], APASS_table['decdeg'][k], 40)
-	intg, intg_unc = estimate_total_light(obj,N_bkg)
+	intg, intg_unc = estimate_total_light(obj,N_bkg, sigma_read, display=False)
 	m_I.append(magnitude(intg))
 	m_APASS.append(float(APASS_table[filt][k]))
 	m_I_unc.append(2.5*intg_unc/intg/log10(e))
@@ -100,11 +86,13 @@ def APASS_zero_points(FM, APASS_table, APASS_rejects, display=False):
   #plt.show()
   # ESTIMATE ZERO POINTS FOR EACH APASS SOURCE
   ZP = array(m_APASS)-array(m_I)
+  print ZP
   # ESTIMATE THE MEAN AND WEIGHTED RMS OF THE ZERO POINTS 
   ZP_mean = sum(ZP)/len(ZP)
   #ZP_rms = sqrt(sum(ZP**2)/len(ZP) - ZP_mean**2)
   weight = 1./np.sqrt(array(m_I_unc)**2+array(m_APASS_unc)**2)
   ZP_rms = sqrt(sum(weight*(ZP-ZP_mean)**2)/sum(weight))
+  print ZP_mean, ZP_rms
   #print '**',ZP_mean, ZP_rms
   # WEIGHTED RMS
   if(display):
@@ -124,7 +112,7 @@ def APASS_zero_points(FM, APASS_table, APASS_rejects, display=False):
 def get_obj_magnitude(obj, N_bkg, sigma_read, ZP_mean, ZP_rms):
 
   # GET EXPOSURE TIME
-  intg, intg_unc = estimate_total_light(obj, N_bkg)
+  intg, intg_unc = estimate_total_light(obj, N_bkg, sigma_read)
 
   # CONVERT TO INSTRUMENTAL MAGNITUDE
   m_I     = magnitude(intg)
@@ -177,29 +165,33 @@ APASS_rejects = [9, 21, 22] # 9 and 21 seem to be at the edge of the field of vi
 def photometry_plot(obj,N_bkg,sigma_read, ZP_mean, ZP_rms):
   m, m_err = get_obj_magnitude(obj, N_bkg, sigma_read, ZP_mean, ZP_rms)
   # INTERMEDIATE STEPS ADDED HERE FOR DISPLAY
+  '''
   pk0 = np.max(obj.image)
-  err0 = sqrt(pk0 + (sigma_read)**2)
   pk1 = pk0 - N_bkg
   m_pk1 = -2.5*log10(pk1/t)
   err1 = err0/t
   m_err1 =  2.5/err0/log(10.)
   m_pk2 = -2.5*log10(pk1/t) + ZP_mean 
   m_err2 = sqrt(m_err1**2 + ZP_rms**2 )
+  '''
+  est,unc = estimate_total_light(obj, N_bkg, sigma_read)
+  m_I = -2.5*log10(est)
+  m_I_err = 2.5*unc/est/log10(e)
 
   col = 'red'
   if(obj.FM.hdulist[0].header['FILTER'] == 'gp'): col='green'
   ax = subplot(511)
   ax.set_yscale('log')
-  errorbar([t_obs],[N_bkg],yerr=[sqrt(N_bkg+sigma_read**2)],fmt='s', color=col, label='QSR peak')
-  errorbar([t_obs],[pk0],yerr=[err0],fmt='*', color=col, label='Background')
-  ylabel('Raw Counts')
+  errorbar([t_obs],[N_bkg],yerr=[np.sqrt(N_bkg+sigma_read**2)],fmt='s', color=col, label='Background')
+  ylabel('Background, Raw Counts')
 
-  subplot(512)
-  errorbar([t_obs], [pk1],yerr=[err0],fmt='o', color=col)
-  ylabel('$N_{pk}-N_{bkg}$,\nRaw Counts')
+  ax1 = subplot(512)
+  #ax1.set_yscale('log')
+  errorbar([t_obs], [est],yerr=[unc],fmt='o', color=col)
+  ylabel('Estimated Illumination from Source,\nRaw Integrated Counts')
 
   subplot(513)
-  errorbar([t_obs], [m_pk1],yerr=[m_err1],fmt='o', color=col)
+  errorbar([t_obs], [m_I],yerr=[m_I_err],fmt='o', color=col)
   ylabel(r'$m_{I}$')
 
   subplot(514)
@@ -226,9 +218,9 @@ def photometry_plot(obj,N_bkg,sigma_read, ZP_mean, ZP_rms):
 count2=0
 #for n in range(0,len(filename_table['filename'])):
 for n in range(0,len(filename_table['filename'])):
-  
   # GET INFORMATION OF FILENAME AND BACKGROUNDS FROM TABLES
   fnm = filename_table['filename'][n]
+  print fnm
   N_bkg = float(filename_table['N_bkg'][n])
   sigma_read = float(filename_table['read_noise'][n])
   print 'Image', n
@@ -236,7 +228,7 @@ for n in range(0,len(filename_table['filename'])):
   mjd_start = 57008.  
   if(filename_table['mjd'][n]< mjd_start):
 	continue
-  if(n==60): break
+  #if(n==60): break
   count2+=1
   # INITIALIZE THE CUSTOMIZED FITS FILE MANAGER
   FM = FITSmanager(dirc+fnm)
@@ -287,41 +279,32 @@ for n in range(0,len(filename_table['filename'])):
 
   ###################################################################################
   obj = SourceImage(FM, ra, dec, 30)  
-  #if(count2==10):
-	#plt.figure()
-	#plt.imshow(obj.image)
-	#plt.show()
   figure(4, figsize=(7,12))
   photometry_plot(obj,N_bkg,sigma_read, ZP_mean, ZP_rms)
-
-  # GET THE QUASAR IMAGE
-  obj = SourceImage(FM, ra_qsr, dec_qsr, 30)
-  figure(5, figsize=(7,12))
-  photometry_plot(obj,N_bkg,sigma_read, ZP_mean, ZP_rms)
   '''
-  if(count2==1):
+  if(count2==9):
 	plt.figure()
-	levels=arange(0,np.max(obj.image-N_bkg),100)[::-1]
-	plt.imshow(obj.image-N_bkg, interpolation='none')
+	plt.imshow(obj.image, interpolation='none')
 	plt.colorbar()
-	cs = plt.contour(obj.image-N_bkg, levels=levels, linewidths=0.5, colors='k')
-	p = cs.collections[0].get_paths()[0]
-	print 'p',p
-	v = p.vertices
-	print 'v',v
-	y = v[:,1]
-	#figure()
-	#plot(v[:,0],v[:,1])
-	print 'y',y
-	print 'sum(abs(y))*0.05', sum(abs(y))*0.05
-	#s[i,j]=sum(abs(y))*0.05
-	#figure()
-	#imshow(p)
-	#plt.colorbar()
-	#print CS
 	plt.show()
   '''
 
+  # GET THE QUASAR IMAGE
+  obj = SourceImage(FM, ra_qsr, dec_qsr, 30)
+  intg, intg_unc = estimate_total_light(obj,N_bkg, sigma_read, display=False)
+
+  figure(5, figsize=(7,12))
+  photometry_plot(obj,N_bkg,sigma_read, ZP_mean, ZP_rms)
+  '''
+  if(count2==11):
+	plt.figure()
+	estimate_total_light(obj, N_bkg, sigma_read, display=True)
+	plt.figure()
+	levels=arange(0,np.max(obj.image),100)[::-1]
+	plt.imshow(obj.image-N_bkg, interpolation='none')
+	plt.colorbar()
+	plt.show()
+  '''
 # FIGURE BEAUTIFICATION
 
 figure(1)
